92e1b67b8a49bb81e6237d3596fe00129c71d708
JOrozco <javier.orozco@alamoconsulting.com>
2018-04-04 12:48:12 +0200

    Uploading the tests with R and R.NET to the repository

---
+++ C#_Example/MyApplication2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MyApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.messagesBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(12, 12);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // messagesBox
+            // 
+            this.messagesBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.messagesBox.Location = new System.Drawing.Point(0, 51);
+            this.messagesBox.Multiline = true;
+            this.messagesBox.Name = "messagesBox";
+            this.messagesBox.Size = new System.Drawing.Size(284, 211);
+            this.messagesBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.messagesBox);
+            this.Controls.Add(this.runButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox messagesBox;
+    }
+}
+
---
+++ C#_Example/MyApplication2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using RDotNet;
+
+namespace MyApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            REngine.SetEnvironmentVariables();
+            // There are several options to initialize the engine, but by default the following suffice:
+            REngine engine = REngine.GetInstance();
+
+            var group2 = engine.Evaluate("source('C:/Users/Alamo/Desktop/Proyectos/PFRunner/Formacion/R_Example/test.r')");
+            var b = engine.GetSymbol("b").AsNumeric();
+            messagesBox.Text = string.Format("Group2: [{0}]", string.Join(", ", group2));
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // you should always dispose of the REngine properly.
+            // After disposing of the engine, you cannot reinitialize nor reuse it
+            REngine engine = REngine.GetInstance();
+            engine.Dispose();
+        }
+    }
+}
